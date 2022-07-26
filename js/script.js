f2eb3f26802ea3b51f34d10e71ac8d21551ed310
FILIPE QUERINO DOS SANTOS <filipe.santos@alunos.sc.senac.br>
2022-07-26 13:40:36 -0300

    Alterado para o front consumir os dados da api.

--- js/script.js
+++ js/script.js
@@ -1,9 +1,45 @@
-const buscaPokemon = async (pokemon) => { 
-    const  ApiResposta = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+const nomePokemon = document.querySelector('.pokemon__name');
+const numeroPokemon = document.querySelector('.pokemon__number');
+const imgPokemon = document.querySelector('.pokemon__image');
 
-    const data = ApiResposta.json();
+const form = document.querySelector('.form');
+const input = document.querySelector('.input__search');
 
-    console.log(data)
+const buttonPrev = document.querySelector('.btn-prev');
+const buttonNext = document.querySelector('.btn-next');
+
+let buscarPokemon = 1;
+
+const buscaPokemon = async (pokemon) => {
+    const ApiResposta = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+
+    if (ApiResposta.status == 200) {
+        const data = ApiResposta.json();
+        return data;
+    }
 }
 
-buscaPokemon();
+const mostraPokemon = async (pokemon) => {
+    nomePokemon.innerHTML = 'Buscando...';
+    numeroPokemon.innerHTML = '';
+
+    const data = await buscaPokemon(pokemon);
+
+    if (data) {
+        nomePokemon.innerHTML = data.name;
+        numeroPokemon.innerHTML = data.id;
+        imgPokemon.src = data.sprites.versions['generation-v']['black-white']['animated']['front_default'];
+
+        input.value = '';
+    } else {
+        nomePokemon.innerHTML = ':(';
+        numeroPokemon.innerHTML = '';
+    }
+}
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    mostraPokemon(input.value.toLowerCase());
+});
+
+mostraPokemon(buscarPokemon);
